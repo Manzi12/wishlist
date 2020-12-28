9863ee075cd61eaabdc1b6dfc9f008d836d52c96
manzi <manzijosephis@gmail.com>
2020-12-28 21:14:38 +0000

    working on sign in and out views with firebase

--- app/src/main/java/org/wit/wishlistapp/MainActivity.kt
+++ app/src/main/java/org/wit/wishlistapp/MainActivity.kt
@@ -1,11 +1,35 @@
 package org.wit.wishlistapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var btn_login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.log_in)
+        auth = FirebaseAuth.getInstance()
+
+
+        btn_login.setOnClickListener{
+            startActivity(Intent(this, Sign_up::class.java))
+            finish()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    fun updateUI(currentUser: FirebaseUser?){}
 }
